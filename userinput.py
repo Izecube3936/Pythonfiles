c06a6986eb4f539187bde523a0129e57cf089aa5
Izecube3936 <fredrik.dahlbom@cmeducations.se>
2026-01-22 00:35:27 +0100

    Basic output
    Input, casting, f-strings with modifier, expressions

---
+++ userinput.py
@@ -0,0 +1,14 @@
+
+#Content: input, casting, f-strings with modifier
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in cm: "))
+#age = age + 1   Koden kommer inte att funka eftersom man inte kan konkatanera strängar
+# age = int(age)  casting, som innebär att vi gör om heltal nu kan lagras i varabeln age istället. 
+#Dock skriver man mindre kod om man på en rad (rad 2) anger att age = int 
+age = age + 1   #Nu fungerar koden som lägger till +1
+print(f"Hello {name}")
+print(f"You are {age} years old")
+print(f"Your height is {height:.2f} cm")    #Placeholder with modifier, dvs. två decimaler
